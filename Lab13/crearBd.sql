c7c827f3c9a47ece1fc1462522f402e6dc59adc6
a01173530 <63377901+a01173530@users.noreply.github.com>
2020-09-28 20:28:40 -0500

    Actualizacion

---
+++ Lab13/crearBd.sql
@@ -0,0 +1,152 @@
+drop TABLE Entregan
+drop TABLE Materiales
+drop TABLE Proyectos
+drop TABLE Proveedores
+
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Materiales')
+
+
+CREATE TABLE Materiales
+(
+  Clave numeric(5) not null,
+  Descripcion varchar(50),
+  Costo numeric (8,2)
+)
+
+BULK INSERT a1173530.a1173530.[Materiales]
+   FROM 'e:\wwwroot\rcortese\materiales.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
+	
+
+SELECT  * FROM Materiales
+
+INSERT INTO Materiales values(1000, 'xxx', 1000)
+SELECT  * FROM Materiales
+
+Delete from Materiales where Clave = 1000 and Costo = 1000
+
+ALTER TABLE Materiales add constraint llaveMateriales PRIMARY KEY (Clave)
+
+INSERT INTO Materiales values(1000, 'xxx', 1000)
+
+sp_helpconstraint materiales
+
+
+
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Proveedores')
+
+CREATE TABLE Proveedores
+(
+  RFC char(13) not null,
+  RazonSocial varchar(50)
+)
+
+ALTER TABLE Proveedores add constraint llaveProveedores PRIMARY KEY (RFC)
+
+BULK INSERT a1173530.a1173530.[Proveedores]
+   FROM 'e:\wwwroot\rcortese\Proveedores.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
+	
+
+SELECT  * FROM Proveedores
+
+
+
+sp_helpconstraint proveedores
+
+
+
+
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Proyectos')
+
+CREATE TABLE Proyectos
+(
+  Numero numeric(5) not null,
+  Denominacion varchar(50)
+)
+
+BULK INSERT a1173530.a1173530.[Proyectos]
+   FROM 'e:\wwwroot\rcortese\Proyectos.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
+SELECT  * FROM Proyectos
+	
+ALTER TABLE Proyectos add constraint llaveProyectos PRIMARY KEY (Numero)
+
+
+sp_helpconstraint proyectos
+
+
+
+
+
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Entregan')
+
+CREATE TABLE Entregan
+(
+  Clave numeric(5) not null,
+  RFC char(13) not null,
+  Numero numeric(5) not null,
+  Fecha DateTime not null,
+  Cantidad numeric (8,2)
+)
+
+
+ALTER TABLE Entregan ADD CONSTRAINT llaveEntregan PRIMARY KEY (Clave,RFC,Numero,Fecha)
+
+
+
+
+sp_helpconstraint entregan
+
+SET DATEFORMAT dmy -- especificar formato de la fecha
+
+BULK INSERT a1173530.a1173530.[Entregan]
+   FROM 'e:\wwwroot\rcortese\Entregan.csv'
+   WITH
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
+
+SELECT  * FROM Entregan
+
+INSERT INTO entregan values (0, 'xxx', 0, '1-jan-02', 0) ;
+
+Delete from Entregan where Clave = 0
+
+ALTER TABLE entregan add constraint cfentreganclave
+foreign key (clave) references materiales(clave);
+
+ALTER TABLE entregan add constraint cfentreganRFC
+foreign key (RFC) references proveedores(RFC);
+
+ALTER TABLE entregan add constraint cfentreganNumero
+foreign key (Numero) references proyectos(Numero);
+
+
+
+INSERT INTO entregan values (1000, 'AAAA800101', 5000, GETDATE(), 0);
+
+Delete from Entregan where Cantidad = 0
+
+ALTER TABLE entregan add constraint cantidad check (cantidad > 0) ;
